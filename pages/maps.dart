f7e9864a6d4f0175a44da8ae33d08b6feb45ca5e
Dilmi01 <118357973+Dilmi01@users.noreply.github.com>
2024-08-05 13:48:49 +0530

    Add files via upload

--- pages/maps.dart
+++ pages/maps.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -12,7 +11,7 @@ class WebViewContainer2 extends StatefulWidget {
 
 class _WebViewContainerState2 extends State<WebViewContainer2> {
 
-  // initialize webview here !!
+
   final controller = WebViewController()
   ..setJavaScriptMode(JavaScriptMode.unrestricted)
   ..loadRequest(Uri.parse('https://www.google.com/maps/place/NSBM+Green+University/@6.8207983,80.0402483,17.98z/data=!4m6!3m5!1s0x3ae2523b05555555:0x546c34cd99f6f488!8m2!3d6.8213291!4d80.0415729!16s%2Fg%2F11h1p1w53r?entry=ttu'));
@@ -20,7 +19,7 @@ class _WebViewContainerState2 extends State<WebViewContainer2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Find Locations"),
+      appBar: AppBar(title: const Text("Find Locations"),
       ),
       body: WebViewWidget(controller: controller),
     );
